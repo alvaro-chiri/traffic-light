cfdbcf6ca901080ffa839411bd0893ee450fdb1b
alvaro-chiri <alvaro.chirinos@yahoo.com>
2022-07-08 00:54:57 +0000

    progress on purple button

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 function Home() {
 	//create your first component
 	const [color, setColor] = useState("null")
+	const [visible, toggleVisibility] = useState(false)
 
 		
 	const cycle = () => {
@@ -23,6 +24,7 @@ function Home() {
 				<div onClick={() => setColor("redLight")} className={(color == "redLight") ? "light redLight selected": "light redLight"}></div>
 				<div onClick={() => setColor("yellowLight")} className={(color == "yellowLight") ? "light yellowLight selected":"light yellowLight"}></div>
 				<div onClick={() => setColor("greenLight")} className={(color == "greenLight") ? "light greenLight selected" : "light greenLight"}></div>
+				<div onClick={() => setColor("purpleLight")} className={(visible == true) ? "light purpleLight selected" : "d-none"}></div>
 				
 			</div>
 			<button onClick={cycle} className="cycleButton">Cycle Light</button>
