319a710228ef96ea363f3c9b3c5669cb6499f042
alvaro-chiri <alvaro.chirinos@yahoo.com>
2022-07-08 00:37:50 +0000

    cycle button done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,9 +4,17 @@ function Home() {
 	//create your first component
 	const [color, setColor] = useState("null")
 
-	// useEffect(() => {}, [color])
-	// console.log(color)
-
+		
+	const cycle = () => {
+		if (color == "redLight") {
+			setColor("greenLight")
+		}
+		else if (color == "greenLight") {
+			setColor("yellowLight")
+		}
+		else {
+			setColor("redLight")
+		};};
 
 	return (
 		<div className="container">
@@ -15,7 +23,9 @@ function Home() {
 				<div onClick={() => setColor("redLight")} className={(color == "redLight") ? "light redLight selected": "light redLight"}></div>
 				<div onClick={() => setColor("yellowLight")} className={(color == "yellowLight") ? "light yellowLight selected":"light yellowLight"}></div>
 				<div onClick={() => setColor("greenLight")} className={(color == "greenLight") ? "light greenLight selected" : "light greenLight"}></div>
+				
 			</div>
+			<button onClick={cycle} className="cycleButton">Cycle Light</button>
 		</div>
 	)
 }
